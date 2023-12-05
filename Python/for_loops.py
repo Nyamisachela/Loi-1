a6045a4e249742520e7a4578b6ce6fb0ecab868f
Nyamisachela <nyamisachelagat@gmail.com>
2023-12-05 16:29:42 +0300

    for loops

--- Python/for_loops.py
+++ Python/for_loops.py
@@ -25,4 +25,31 @@ my_float = float(my_float) #convert from string to floating point number
 
 print("Round to 2 decimals : {:.2f}".format(my_float))
 
+#demostrate compounding interest
+
+#enter investenent amount and expected interest
+
+#each year the investement will increase by their initial investemnt + initial investemnt * interest rate
+#print out the earnings
+
+# first ask for the initial invested and interest rate
+
+money = input("Investment amount : ")
+interest_rate = input("Interest rate : ")
+#convert the value to a float
+money = float(money)
+#convert value to float and round of the percentange rate by 2 digits
+interest_rate = float(interest_rate) * .01
+
+#cycle through 10 years using for loop and range 0 -9
+for i in range(10):
+    money = money + (money * interest_rate)
+
+#output results
+print("Investement after 10 years : {:.2f}".format(money))
+
+
+
+
+
 
